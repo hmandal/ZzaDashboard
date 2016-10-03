7f36b9a19701e3c083c1d3027e13bb2124d3418d
Himanshu Mandal <hmandal007@gmail.com>
2016-10-03 10:20:25 +0530

    v1.0

---
+++ ZzaDesktop/Customers/AddEditCustomerViewModel.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zza.Data;
+using ZzaDesktop.Services;
+
+namespace ZzaDesktop.Customers
+{
+    class AddEditCustomerViewModel : BindableBase
+    {
+        public AddEditCustomerViewModel()
+        {
+            CancelCommand = new RelayCommand(OnCancel);
+            SaveCommand = new RelayCommand(OnSave, CanSave);
+        }
+        private bool _EditMode;
+        public bool EditMode
+        {
+            get { return _EditMode; }
+            set { SetProperty(ref _EditMode, value); }
+        }
+
+        private SimpleEditableCustomer _Customer;
+        public SimpleEditableCustomer Customer
+        {
+            get { return _Customer; }
+            set { SetProperty(ref _Customer, value); }
+        }
+
+        private Customer _editingCustomer = null;
+
+        public void SetCustomer(Customer cust)
+        {
+            _editingCustomer = cust;
+            Customer = new SimpleEditableCustomer();
+            CopyCustomer(cust, Customer);
+        }
+
+        public RelayCommand CancelCommand { get; private set; }
+        public RelayCommand SaveCommand { get; private set; }
+
+        public event Action Done = delegate { };
+
+
+        private void OnCancel()
+        {
+            Done();
+        }
+
+        private void OnSave()
+        {
+            Done();
+        }
+
+        private void UpdateCustomer(SimpleEditableCustomer source, Customer target)
+        {
+            target.FirstName = source.FirstName;
+            target.LastName = source.LastName;
+            target.Phone = source.Phone;
+            target.Email = source.Email;
+        }
+
+        private bool CanSave()
+        {
+            return true;
+        }
+
+        private void CopyCustomer(Customer source, SimpleEditableCustomer target)
+        {
+            target.Id = source.Id;
+            if (EditMode)
+            {
+                target.FirstName = source.FirstName;
+                target.LastName = source.LastName;
+                target.Phone = source.Phone;
+                target.Email = source.Email;
+            }
+        }
+    }
+}
---
+++ ZzaDesktop/Customers/SimpleEditableCustomer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZzaDesktop.Customers
+{
+    class SimpleEditableCustomer : BindableBase
+    {
+
+        private Guid _Id;
+
+        public Guid Id
+        {
+            get { return _Id; }
+            set { SetProperty(ref _Id, value); }
+        }
+
+
+        private string _FirstName;
+
+        public string FirstName
+        {
+            get { return _FirstName; }
+            set { SetProperty(ref _FirstName, value); }
+        }
+
+
+        private string _LastName;
+
+        public string LastName
+        {
+            get { return _LastName; }
+            set { SetProperty(ref _LastName, value); }
+        }
+
+
+        private string _Email;
+
+        public string Email
+        {
+            get { return _Email; }
+            set { SetProperty(ref _Email, value); }
+        }
+
+
+        private string _Phone;
+
+        public string Phone
+        {
+            get { return _Phone; }
+            set { SetProperty(ref _Phone, value); }
+        }
+    }
+}
